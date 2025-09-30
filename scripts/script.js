f04986348afd6c9d71a39d67ba302cc320406dd4
niloufar88 <niloufar88@hotmail.com>
2025-09-30 20:39:58 +0330

    final changes

--- scripts/script.js
+++ scripts/script.js
@@ -1,93 +1,195 @@
-let showMoreBtn = document.getElementById('show_more');
-let currentItem = 5;
+const formEl = document.querySelector('.search');
+const inputEl = document.getElementById('search-input');
+const cardsContainerEl = document.querySelector('.cards_container');
+const cardsEl = document.querySelector('cards');
+const pokedexEl = document.getElementById('logo_container');
+const overlayEl = document.querySelector('.card_overlay');
+const searchBtn = document.querySelector('.searchBtn');
+const bodyEl = document.getElementById('body');
+
+const fetchedUrls = [];
+const showMoreBtn = document.getElementById('show_more');
+let currentItem = 20;
+let inputData = "";
+let currentPokemonId;
 
 
-
-async function onLoad() {
-    await getUrlDataFromApi();
-}
-
-async function getUrlDataFromApi() {
-    const response = await fetch(baseUrl);
-    const responseToJson = await response.json();
-
-    for (let index = 0; index < responseToJson.results.length; index++) {
-        urlElement.push(responseToJson.results[index].url);
-    }
-    fetchAllUrls();
-}
-
-
-async function fetchAllUrls() {
-    const response = await Promise.all(urlElement.map(url => fetch(url)));
-    const responseToJson = await Promise.all(response.map(res => res.json()));
-    console.log(responseToJson);
-
-    for (let urlIndex = 0; urlIndex < responseToJson.length; urlIndex++) {
-        urlIntoObject.push(responseToJson[urlIndex]);
-        idEl.push(urlIntoObject[urlIndex].id);
-        pokeNames.push(urlIntoObject[urlIndex].name);
-        pokemonCardImg.push(urlIntoObject[urlIndex].sprites.other.home.front_default);
-        pokeType.push(urlIntoObject[urlIndex].types[0].type.name);
-        pokeAbilitiesEl.push(urlIntoObject[urlIndex].abilities);
-    }
-    console.log(pokeType);
-    console.log(idEl);
-    renderCardData();
-}
-
-async function renderCardData() {
-    cardsContainerEl.innerHTML = "";
-    for (let i = 0; i < urlIntoObject.length; i++) {
-        cardsContainerEl.innerHTML += getDataFromUrlTemplate(i);
-    }
-}
-
+// load more function:
 showMoreBtn.addEventListener('click', () => {
     let cards = [...document.querySelectorAll('.main .cards_container .cards')];
-    for (let i = currentItem; i < currentItem + 5; i++) {
+    for (let i = currentItem; i < currentItem + 20; i++) {
         cards[i].style.display = "flex";
     }
-    currentItem += 5;
+    currentItem += 20;
     if (currentItem >= cards.length) {
-        showMoreBtn.innerHTML = `Show Less!`;
+        showMoreBtn.style.display = "none";
     }
 })
+
+// Event Listeners:
+formEl.addEventListener('submit', (e) => {
+    e.preventDefault();
+    cardsContainerEl.innerHTML = "";
+    inputData = inputEl.value.toLowerCase();
+    let filteredPokemons;
+    if (inputData.length >= 3) {
+        filteredPokemons = fetchedUrls.filter((pokemon) => {
+            return pokemon.name.toLowerCase().startsWith(inputData);
+        });
+    }
+    if (filteredPokemons.length > 0) {
+        cardsContainerEl.innerHTML = "";
+        filteredPokemons.forEach(pokemon => {
+            cardsContainerEl.innerHTML += getDataFromUrlTemplate(pokemon);
+            imgLooping();
+        });
+        showMoreBtn.style.display = "none";
+    } else {
+        cardsContainerEl.innerHTML = `<h2>Oops! Pokemon not Found</h2>`;
+        errorHandling();
+        return;
+    }
+    cardsContainerEl.classList.remove('fetchFailed');
+    inputEl.value = "";
+})
+
+
+const imgLooping = () => {
+    const cardImgEl = document.querySelectorAll('.pokemon_img');
+    cardImgEl.forEach(img => {
+        img.addEventListener("click", (e) => {
+            popupOverlayDetails(e);
+        });
+    });
+}
+
+pokedexEl.addEventListener('click', () => {
+    fetchPokemons();
+})
+
+//fetch urls on page load:
+const fetchPokemons = async() => {
+    try {
+        cardsContainerEl.innerHTML = `<h2>rendering fetched Data ... please wait</h2>`;
+        showMoreBtn.style.display = "none";
+        for (let i = 1; i <= 160; i++) {
+            const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${i}`);
+            const responseToJson = await response.json();
+            fetchedUrls.push(responseToJson);
+        }
+        cardsContainerEl.classList.remove('fetchFailed');
+        console.log(fetchedUrls);
+        renderCardData();
+    } catch (error) {
+        cardsContainerEl.innerHTML = `<h2> Oops! Error by fetching Urls ...</h2>`;
+    }
+}
+
+// render pokemon data:
+const renderCardData = async() => {
+    cardsContainerEl.innerHTML = "";
+    fetchedUrls.map((poke) => {
+        cardsContainerEl.classList.remove('fetchFailed');
+        cardsContainerEl.innerHTML += getDataFromUrlTemplate(poke);
+    })
+    showMoreBtn.style.display = "block";
+    imgLooping();
+}
+
+
+//overlay Function:
+const popupOverlayDetails = async(e) => {
+    e.preventDefault();
+    try {
+        let cardItem = e.target.closest('.cards');
+        const cardId = cardItem ? cardItem.dataset.id : null;
+        // console.log("Card ID:", cardId);
+        if (cardId) {
+            const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${cardId}`);
+            const data = await response.json();
+            cardsContainerEl.classList.remove('fetchFailed');
+            renderOverlayCards(data);
+        } else {
+            cardsContainerEl.innerHTML = `<h2>Oops! Pokemon not Found</h2>`;
+            cardsContainerEl.classList.add('fetchFailed');
+        }
+    } catch (error) {
+        cardsContainerEl.innerHTML = `oops, Error by fetching Pokemon :(`;
+        errorHandling();
+    }
+}
+
+async function navigationFunctions(direction) {
+    if (direction === "left" && currentPokemonId > 1) {
+        currentPokemonId--;
+        await repeatedFetch(currentPokemonId);
+    }
+    if (direction === "right" && currentPokemonId < 160) {
+        currentPokemonId++;
+        await repeatedFetch(currentPokemonId);
+    }
+}
+
+async function repeatedFetch(id) {
+    const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${id}`);
+    const data = await response.json();
+    cardsContainerEl.classList.remove('fetchFailed');
+    renderOverlayCards(data);
+}
+
+function renderOverlayCards(data) {
+    currentPokemonId = data.id;
+    cardsContainerEl.classList.remove('fetchFailed');
+    overlayEl.innerHTML = getDataForOverlayTemplate(data);
+    document.getElementById('card_overlay').classList.remove('d-none');
+    bodyEl.classList.add('no-scroll');
+}
+
+//buttons Event Listeners:
+const leftArrowbBtn = document.getElementById('leftArrow');
+if (leftArrowbBtn) {
+    leftArrowbBtn.addEventListener("click", () => {
+        if (currentPokemonId > 1) {
+            navigationFunctions("left");
+        }
+    });
+}
+
+const rightArrowBtn = document.getElementById('rightArrow');
+if (rightArrowBtn) {
+    rightArrowBtn.addEventListener("click", () => {
+        if (currentPokemonId < 160) {
+            navigationFunctions("right");
+        }
+    });
+}
+
+window.history.pushState({}, "", `./index.html?id=${currentPokemonId}`);
 
 function toggleNoneDisplayNavbar(id1, id2, id3, id4, id5, id6) {
     document.getElementById(id1).style.display = "none";
     document.getElementById(id2).style.display = "none";
     document.getElementById(id3).style.display = "block";
-    document.getElementById(id3).style.backgroundColor = "#6c9f9fff";
-    document.getElementById(id4).style.backgroundColor = "#6c9f9fff";
+    document.getElementById(id3).style.backgroundColor = "#ed9b1fff";
+    document.getElementById(id4).style.backgroundColor = "#ed9b1fff";
     document.getElementById(id5).style.backgroundColor = "#ffebcd";
     document.getElementById(id6).style.backgroundColor = "#ffebcd";
 }
 
-// function overlayDisplayAdd(elementId) {
-//     document.getElementById('card_overlay').classList.add('d-none');
-//     stopPropagationFurthur(elementId);
-// }
 
-// function overlayDisplayRemove(elementId) {
-//     document.getElementById('card_overlay').classList.remove('d-none');
-//     stopPropagationFurthur(elementId);
-// }
+function overlayDisplayAdd(elementId) {
+    document.getElementById('card_overlay').classList.add('d-none');
+    bodyEl.classList.remove('no-scroll');
+    stopPropagationFurthur(elementId);
+}
 
-// function stopPropagationFurthur(elementId) {
-//     document.getElementById(elementId).addEventListener("click", (event) => {
-//         event.stopPropagation();
-//     })
-// }
+function stopPropagationFurthur(elementId) {
+    document.getElementById(elementId).addEventListener("click", (event) => {
+        event.stopPropagation();
+    })
+}
 
-
-
-
-// async function getOverlayData() {
-//     const response = await Promise.all(urlElement.map(url => fetch(url)));
-//     const responseToJson = await Promise.all(response.map(res => res.json()));
-//     // console.log(responseToJson);
-//     responseToJson.forEach((query) => {
-//         overlayEl.innerHTML += getDataForOverlayTemplate(query);;
-//     })
-// }
+function errorHandling() {
+    cardsContainerEl.classList.add('fetchFailed');
+    showMoreBtn.style.display = "none";
+}
--- scripts/templates.js
+++ scripts/templates.js
@@ -1,83 +1,107 @@
-function getDataFromUrlTemplate(i) {
+function getDataFromUrlTemplate(poke) {
     return `
-        <div class="cards">
+        <div class="cards" data-id="${poke.id}" id="cards">
             <div class="header">
-              <span class="pokemon_id"># ${urlIntoObject[i].id}</span>
-              <h3 class="pokemon_name">${urlIntoObject[i].name}</h3>
+              <span class="pokemon_id"># ${poke.id}</span>
+              <h3 class="pokemon_name">${poke.name}</h3>
             </div>
-            <div class="pokemon_img">
-              <img src="${urlIntoObject[i].sprites.other.home.front_default}" alt="${urlIntoObject[i].name}" />
-            </div>
+            <div class="pokemon_img bg_${poke.types[0].type.name}">
+              <img src="${poke.sprites.other.dream_world.front_default}" alt="${poke.name}"/></div> 
 
             <div class="pokemon_type">
-              <button class="symbol_type">${urlIntoObject[i].types[0].type.name}</button>
+            <span class="symbol_type">${poke.types[0].type.name}</span> <span class="symbol_type">${poke.types[1] !==undefined? poke.types[1].type.name: ""}</span>
+              
             </div>
-          </div>  
+            
+          </div>
+           
     `;
 }
 
-// function getDataForOverlayTemplate(query) {
-//     return `
-//   <div
-//             class="overlay"
-//             id="overlay"
-//             onclick="stopPropagationFurthur('overlay')"
-//           >
-//             <div class="overlay_header">
-//               <span class="overlay_header_id"># ${query.id}</span>
-//               <h3 class="overlay_header_name">${query.name}</h3>
-//             </div>
-//             <div class="overlay_img">
-//               <img src="${query.sprites.other.home.front_default}" alt="${query.name}" />
-//             </div>
-//             <div class="overlay_navbar">
-//               <a
-//                 class="overlay_list"
-//                 id="main_list"
-//                 onclick="toggleNoneDisplayNavbar('moves_container', 'stats_container', 'main_container', 'main_list','stats_list','moves_list')"
-//               >
-//                 Main
-//               </a>
-//               <a
-//                 class="overlay_list"
-//                 id="stats_list"
-//                 onclick="toggleNoneDisplayNavbar('main_container', 'moves_container', 'stats_container','stats_list','main_list','moves_list')"
-//               >
-//                 Stats
-//               </a>
-//               <a
-//                 class="overlay_list"
-//                 id="moves_list"
-//                 onclick="toggleNoneDisplayNavbar('main_container', 'stats_container', 'moves_container','moves_list','main_list','stats_list')"
-//               >
-//                 Moves
-//               </a>
-//             </div>
-//             <div class="overlay_navbar_container" id="main_container">
-//               <h4>HEIGHT = ${query.height}</h4>
-//               <h4>WEIGHT = ${query.weight}</h4>
-//               <h4>BASE-EXPERIENCE = ${query.base_experience}</h4>
-//               <h4>ABILITIES = ${query.abilities[0].ability.name} // ${query.abilities[1].ability.name} </h4>
-//             </div>
-//             <div class="overlay_navbar_container d-none" id="stats_container">
-//               <h4>HP = ${query.stats[0].base_stat}/100</h4>
-//               <h4>ATTACK = ${query.stats[1].base_stat}/100</h4>
-//               <h4>DEFENCE = ${query.stats[2].base_stat}/100</h4>
-//               <h4>SP-ATTACK = ${query.stats[3].base_stat}/100</h4>
-//               <h4>SP-ADEFENCE = ${query.stats[4].base_stat}/100</h4>
-//               <h4>SPEED = ${query.stats[5].base_stat}/100</h4>
-//             </div>
-//             <div class="overlay_navbar_container d-none" id="moves_container">
-//               <button>${query.moves[0].move.name}</button>
-//               <button>${query.moves[1].move.name}</button>
-//               <button>${query.moves[2].move.name}</button>
-//               <button>${query.moves[3].move.name}</button>
-//               <button>${query.moves[4].move.name}</button>
-//               <button>${query.moves[5].move.name}</button>
-//               <button>${query.moves[6].move.name}</button>
-//               <button>${query.moves[7].move.name}</button>
-//               <button>${query.moves[8].move.name}</button>
-//             </div>
-//           </div>
-//   `
-// }
+// const move1 = poke.moves && poke.moves[0] && poke.moves[0].move ? poke.moves[0].move.name : "N/A";
+// const move2 = poke.moves && poke.moves[1] && poke.moves[1].move ? poke.moves[1].move.name : "N/A";
+
+function getDataForOverlayTemplate(data) {
+    return `
+  <div
+            class="overlay"
+            id="overlay" onclick="stopPropagationFurthur('overlay')"
+          >
+            <div class="overlay_header" id="overlay_header" onclick="stopPropagationFurthur('overlay')">
+              <span class="overlay_header_id"># ${data.id}</span>
+              <h3 class="overlay_header_name">${data.name}</h3>
+            </div>
+            <div class="poke_img">
+              <a href="#" class="arrow left-arrow"  onclick="stopPropagationFurthur('overlay')">
+                  <img src="assets/icons/chevron_left.svg" alt="" id="leftArrow" onclick="navigationFunctions('left')"/>
+              </a>
+            <div class="overlay_img bg_${data.types[0].type.name}" id="overlay_img" onclick="stopPropagationFurthur('overlay')">
+              <img src="${data.sprites.other.dream_world.front_default}" alt="${data.name}" />
+              </div>
+                <a href="#" class="arrow right-arrow"  onclick="stopPropagationFurthur('overlay')">
+                    <img src="assets/icons/chevron_right.svg" alt="" id="rightArrow" onclick="navigationFunctions('right')"/>
+                </a>
+            </div>
+            <div class="overlay_navbar" id="overlay_navbar" onclick="stopPropagationFurthur('overlay')">
+              <a
+                class="overlay_list"
+                id="main_list"
+                onclick="toggleNoneDisplayNavbar('gens_container', 'stats_container', 'main_container', 'main_list','stats_list','Evo_list')"
+              >
+                Main
+              </a>
+              <a
+                class="overlay_list"
+                id="stats_list"
+                onclick="toggleNoneDisplayNavbar('main_container', 'gens_container', 'stats_container','stats_list','main_list','Evo_list')"
+              >
+                Stats
+              </a>
+              <a
+                class="overlay_list"
+                id="Evo_list"
+                onclick="toggleNoneDisplayNavbar('main_container', 'stats_container', 'gens_container','Evo_list','main_list','stats_list')"
+              >
+                Evo
+              </a>
+            </div>
+            <div class="overlay_navbar_container" id="main_container">
+              <h4>HEIGHT = ${(data.height) / 10} m</h4>
+              <h4>WEIGHT = ${(data.weight) / 10} kg</h4>
+              <h4>BASE-EXPERIENCE = ${data.base_experience}</h4>
+              <h4>ABILITIES = ${data.abilities.map((ability) => ability.ability.name).join(' , ')} </h4>
+            </div>
+            <div class="overlay_navbar_container stats_container d-none" id="stats_container"> 
+            <div class="stats">
+            <h4>HP</h4>
+            <progress value="${data.stats[0].base_stat}" max="100" class="progress-bar"></progress>
+            </div>
+            <div class="stats">
+            <h4>ATTACK</h4>
+            <progress value="${data.stats[1].base_stat}" max="100" class="progress-bar"></progress>
+            </div>
+            <div class="stats">
+            <h4>DEFENCE</h4>
+            <progress value="${data.stats[2].base_stat}" max="100" class="progress-bar"></progress>
+            </div>
+            <div class="stats">
+            <h4>SP-ATTACK</h4>
+            <progress value="${data.stats[3].base_stat}" max="100" class="progress-bar"></progress>
+            </div>
+            <div class="stats">
+            <h4>SP-DEFENCE</h4>
+            <progress value="${data.stats[4].base_stat}" max="100" class="progress-bar"></progress>
+            </div>
+            <div class="stats">
+            <h4>SPEED</h4>
+            <progress value="${data.stats[5].base_stat}" max="100" class="progress-bar"></progress>
+            </div>
+              
+            </div>
+            <div class="overlay_navbar_container generation d-none" id="gens_container">
+              <div class="gen_container"><img src="${data.sprites.versions["generation-i"]["red-blue"].front_default}" alt="generation-i" class="genImg" /> <h4>generation- i</h4> </div>
+              <div class="gen_container"><img src="${data.sprites.versions["generation-ii"]["gold"].front_default}" alt="generation-ii" class="genImg" /> <h4>generation- ii</h4></div>
+              
+            </div>
+  </div>`;
+}
